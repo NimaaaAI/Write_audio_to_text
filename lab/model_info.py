d2706b2267885730757baf683c35d7343220c4bf
nima <nima.saghi1993@gmail.com>
2026-09-26 11:19:19 +0200

    Finish model_info script

--- lab/model_info.py
+++ lab/model_info.py
@@ -6,16 +6,6 @@ for i in model_name:
     model_name = i
     print(f"\n{model_name}: \n")
     info = api.model_info(model_name, files_metadata=True)
-    # print("\nDecoder Model:\n")
-    # for i in info.siblings:
-    #     if i.rfilename.startswith("onnx/decoder_model"):
-    #         #rint(f"{i.rfilename}, {i.size / 1000000:.1f}")
-    #         pass
-    # print("\nEncoder Model:\n")
-    # for j in info.siblings:
-    #     if j.rfilename.startswith("onnx/encoder_model"):
-    #         print(f"{j.rfilename}, {j.size / 1000000:.1f}")
-    #         pass
 
     encoders = {}
     decoders = {}
@@ -29,8 +19,6 @@ for i in model_name:
         elif name.startswith("onnx/decoder_model_merged"):
             pr = name.removeprefix("onnx/decoder_model_merged").removesuffix(".onnx")
             decoders[pr] = mb
-    #print(f"\nencoders:, {encoders}")
-    #print(f"\ndecoders: , {decoders}")
 
     for pr in encoders:
         if pr in decoders:
